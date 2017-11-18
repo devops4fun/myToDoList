76852e115e4fc0bebd142e7d8f328cb61919467d
Morizel Hodges <Mo@Morizel-Hodgess-MacBook-Pro.local>
2017-11-18 18:31:34 -0500

    added a down button to the list items

--- js/app.js
+++ js/app.js
@@ -11,9 +11,29 @@ const descriptionButtonWarning = document.querySelector('p.descriptionButtonWarn
 
        listUl.addEventListener('click', (event) => {   
           if (event.target.tagName == 'BUTTON'){
+            
+            if(event.target.className == 'remove'){
             let li = event.target.parentNode;
             let ul = li.parentNode;
             ul.removeChild(li);
+          }
+
+            if(event.target.className == 'up'){
+            let li = event.target.parentNode;
+            let prevLi = li.previousElementSibling;
+            let ul = li.parentNode;
+            if(prevLi){
+            ul.insertBefore(li, prevLi);
+                      }
+          }
+            if(event.target.className == 'down'){
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            if(li.nextElementSibling){
+            ul.insertBefore(li.nextElementSibling, li);
+
+              }
+          } 
         }
       });
 
