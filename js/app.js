142900813cdc720f4b6a5badb9fd85023e07f68c
Morizel Hodges <Mo@Morizel-Hodgess-MacBook-Pro.local>
2017-11-18 00:12:10 -0500

    myToDOList

---
+++ js/app.js
@@ -0,0 +1,64 @@
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
+const addItemButtonWarning = document.querySelector('p.addItemButtonWarning');
+const mouseEnter = document.getElementById('mylist');
+const mouseOut = document.getElementById('mylist');
+
+//hide the add item warning button
+addItemButtonWarning.style.display = 'none';
+
+toggleList.addEventListener('click', () =>{
+            if(listDiv.style.display == 'none') {
+                toggleList.textContent = 'Hide list';
+                listDiv.style.display = 'block';
+}
+               else { 
+                      listDiv.style.display = 'none';
+                      toggleList.textContent = 'Show list';
+                    }
+                            });
+descriptionButton.addEventListener('click', () => {
+                        descriptionP.innerHTML = descriptionInput.value + ':'
+                                   descriptionInput.value = '';
+                                   
+                                   });
+
+addItemButton.addEventListener('click', () => {
+                               if(!(addItemInput.value == '' )){
+                               
+                               addItemButtonWarning.style.display = 'none';
+                               let ul = document.getElementsByTagName('ul')[0];
+                               let li = document.createElement('li');
+                               li.textContent = addItemInput.value;
+                               ul.appendChild(li);
+                               addItemInput.value = '';
+}else {
+    addItemButtonWarning.style.color = 'Red';
+    addItemButtonWarning.style.display = 'block';
+      }
+                               });
+
+removeItemButton.addEventListener('click', () => {
+                               {
+                               let ul = document.getElementsByTagName('ul')[0];
+                               let li = document.querySelector('li:last-child');
+                               ul.removeChild(li);
+                                }
+});
+
+  mouseEnter.addEventListener("mouseenter", ( event ) => {   
+    // highlight the mouseenter target
+      event.target.style.color = "#508abc";
+      event.target.style.textDecoration = "underline";
+  });
+
+  mouseOut.addEventListener("mouseout", ( event ) => {
+      event.target.style.color = "#484848";
+      event.target.style.textDecoration = "none";
+});
